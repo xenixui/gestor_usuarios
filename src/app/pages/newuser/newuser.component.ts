281d5dde6c2d5d8b6ed37e9d499ef3256ec6f387
xenixux <kseniakonnova.ux@gmail.com>
2026-03-08 12:28:44 +0100

    Components and roots creation finished

---
+++ src/app/pages/newuser/newuser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newuser',
+  imports: [],
+  templateUrl: './newuser.component.html',
+  styleUrl: './newuser.component.css',
+})
+export class NewuserComponent {
+
+}
---
+++ src/app/pages/updateuser/updateuser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-updateuser',
+  imports: [],
+  templateUrl: './updateuser.component.html',
+  styleUrl: './updateuser.component.css',
+})
+export class UpdateuserComponent {
+
+}
