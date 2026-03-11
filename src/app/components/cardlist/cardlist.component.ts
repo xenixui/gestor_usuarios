b3c6c296a478cf67754dba78f1dfdf9f1521d136
Ksenia Konnova <kseniakonnova.ux@gmail.com>
2026-03-11 17:33:06 +0100

    Header & Footer front, HomeComponent front

---
+++ src/app/components/cardlist/cardlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cardlist',
+  imports: [],
+  templateUrl: './cardlist.component.html',
+  styleUrl: './cardlist.component.css',
+})
+export class CardlistComponent {
+
+}
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '../../../../node_modules/@angular/router/types/_router_module-chunk';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
