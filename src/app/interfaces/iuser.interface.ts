5aed7d6aeb18b85306cf52ddd9ed5052f14cb83b
Ksenia Konnova <kseniakonnova.ux@gmail.com>
2026-03-13 15:50:03 +0100

    Interfaces created. Service WIP.

--- src/app/interfaces/iuser.interface.ts
+++ src/app/interfaces/iuser.interface.ts
@@ -1,2 +1,19 @@
-export interface IUser {
+export interface IResponse {
+    page:        number;
+    per_page:    number;
+    total:       number;
+    total_pages: number;
+    data:     IUser[];
 }
+
+export interface IUser {
+    _id: string;
+    id: number;
+    first_name: string;
+    last_name: string;
+    username: string;
+    email: string;
+    image: string;
+    password: string;
+}
+
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IResponse, IUser } from '../interfaces/iuser.interface';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private httpClient = inject(HttpClient);
+
+  private baseUrl: string = "https://peticiones.online/users";
+
+  getAll(): Promise<IResponse> {
+    return lastValueFrom(this.httpClient.get<IResponse>(this.baseUrl))
+  }
+
+  getByID(id:string): Promise<IUser> {
+    return lastValueFrom(this.httpClient.get<IUser>(`${this.baseUrl}/${id}`))
+  }
+}
