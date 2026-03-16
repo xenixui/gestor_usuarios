2b8293bd33c23485c7a5666c479e04915374a1c1
Ksenia Konnova <kseniakonnova.ux@gmail.com>
2026-03-16 17:24:10 +0100

    Pagination finished

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { CardlistComponent} from '../../components/cardlist/cardlist.component';
 import { HerosectionComponent } from '../../shared/herosection/herosection.component';
 import { TitlesectionComponent } from '../../shared/titlesection/titlesection.component';
-import { PaginatorComponent } from '../../components/paginator/paginator.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, CardlistComponent, HerosectionComponent, TitlesectionComponent, PaginatorComponent],
+  imports: [CardlistComponent, HerosectionComponent, TitlesectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
