eea963ca1ff74b12c8ca7660d3ea5157a2bf5c3a
Ksenia Konnova <kseniakonnova.ux@gmail.com>
2026-03-16 17:43:58 +0100

    User view WIP

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { TitlesectionComponent } from "../../shared/titlesection/titlesection.component";
+import { UsersService } from '../../services/users.service';
+import { IUser } from '../../interfaces/iuser.interface';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [TitlesectionComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  _id = input<string>();
+   userService = inject(UsersService);
+   user = signal<IUser | null>(null);
 
+  ngOnInit() {
+    console.log(this._id());
+  }
 }
