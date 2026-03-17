d64d6cd4ca7a1cdda2bf35080b0efdb4dd376789
Ksenia Konnova <kseniakonnova.ux@gmail.com>
2026-03-17 14:53:40 +0100

    User view finished

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -2,19 +2,24 @@ import { Component, inject, input, signal } from '@angular/core';
 import { TitlesectionComponent } from "../../shared/titlesection/titlesection.component";
 import { UsersService } from '../../services/users.service';
 import { IUser } from '../../interfaces/iuser.interface';
+import { RouterLink } from '@angular/router';
+import { ListitemComponent } from "../../components/listitem/listitem.component";
 
 @Component({
   selector: 'app-user',
-  imports: [TitlesectionComponent],
+  imports: [TitlesectionComponent, RouterLink, ListitemComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
   _id = input<string>();
-   userService = inject(UsersService);
-   user = signal<IUser | null>(null);
+  userService = inject(UsersService);
+  user = signal<IUser | null>(null);
 
-  ngOnInit() {
-    console.log(this._id());
+  async ngOnInit() {
+    const id = this._id();
+    if(id) {
+      this.user.set(await this.userService.getByID(id));
+    }
   }
 }
